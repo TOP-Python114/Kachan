66a06b69adafb2cfa8a6b593cb4e89aa3d96231c
Gennadiy <renardes@gmail.com>
2022-10-10 13:05:24 +0500

    09.11: правки удобства читаемости кода

--- 09.11/1.1.py
+++ 09.11/1.1.py
@@ -1,14 +1,8 @@
-#Вам даны классы Note, ScoreNote и MIDINote. В файле constants.py прописаны используемые в перечислителях константы.
-#прототип
-#Допишите конструкторы классов ScoreNote и MIDINote исходя из требований о наличии дополнительных атрибутов
-#у соответствующих классов. Требования подписаны в виде комментариев под сигнатурами классов.
-# Допишите метод clone() в родительском классе. В методе необходимо учесть наследование.То есть, метод
-#должен клонировать не только экземпляр класса Note, но и экземпляры дочерних классов, создавая новые
-#экземпляры тех же классов.Также, необходимо добавить возможность переопределять значения
-#некоторых атрибутов при клонировании.
-#Например:
+# Вам даны классы Note, ScoreNote и MIDINote. В файле constants.py прописаны используемые в перечислителях константы.
+# Допишите конструкторы классов ScoreNote и MIDINote исходя из требований о наличии дополнительных атрибутов у соответствующих классов. Требования подписаны в виде комментариев под сигнатурами классов.
+# Допишите метод clone() в родительском классе. В методе необходимо учесть наследование. То есть, метод должен клонировать не только экземпляр класса Note, но и экземпляры дочерних классов, создавая новые экземпляры тех же классов.
+# Также, необходимо добавить возможность переопределять значения некоторых атрибутов при клонировании.
 
-from dataclasses import dataclass
 from copy import deepcopy
 from constants import *
 
@@ -27,9 +21,8 @@ class Note:
         self.accidental = accidental
         self.duration = duration
 
-    def clone(self,**kwargs):
-        """Клонирование и обновление словаря атребутов"""
-
+    def clone(self, **kwargs):
+        """Клонирование и обновление словаря атрибутов."""
         clone_obj = deepcopy(self)
         clone_obj.__dict__.update(**kwargs)
         return clone_obj
@@ -65,9 +58,13 @@ class MIDINote(Note):
         self.velocity = velocity
 
 
-pot = Note(pitch=Pitch.C, octave=Octave.S_CONTRA,accidental=Accidental.NATURAL)
+pot = Note(pitch=Pitch.C, octave=Octave.S_CONTRA, accidental=Accidental.NATURAL)
 
-midi_c3 = MIDINote(pitch=Pitch.C, octave=Octave.LINE_1, velocity = 80)
-midi_d3 = midi_c3.clone(pitch=Pitch.D,octave=Octave.S_CONTRA)
+midi_c3 = MIDINote(pitch=Pitch.C, octave=Octave.LINE_1, velocity=80)
+midi_d3 = midi_c3.clone(pitch=Pitch.D, octave=Octave.S_CONTRA)
 
 print(midi_d3.clone().octave)
+
+
+# ДОБАВИТЬ: под меткой tests закомментированные результаты выполнения скрипта с различными входными данными
+# tests:
