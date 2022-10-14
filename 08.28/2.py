306af5c3eb3eebcbb85efbbc53dddc0284968cda
Gennadiy <renardes@gmail.com>
2022-10-14 15:51:47 +0500

    08.28: правки удобства читаемости

--- 08.28/2.py
+++ 08.28/2.py
@@ -1,12 +1,9 @@
-
-#Для задачи №67 из Стивенсона реализуйте объектную модель из трёх классов: Polygon, LineSegment, Point.
-#Используйте композицию. Для расчёта значений используйте геттеры/сеттеры.
-#Начните с запроса у пользователя координат x и y первой точки многоугольника.Продолжайте
-#запрашивать координаты следующих точек фигуры, пока пользователь
-#не оставит строку ввода координаты по оси x пустой
+"""Для задачи №67 из Стивенсона реализуйте объектную модель из трёх классов: Polygon, LineSegment, Point. Используйте композицию. Для расчёта значений используйте геттеры/сеттеры. Начните с запроса у пользователя координат x и y первой точки многоугольника. Продолжайте запрашивать координаты следующих точек фигуры, пока пользователь не оставит строку ввода координаты по оси x пустой"""
 
 from dataclasses import dataclass
 from itertools import pairwise
+
+
 @dataclass()
 class Point:
     x: int
@@ -15,7 +12,6 @@ class Point:
 
 @dataclass
 class LineSegment:
-
     """Определяет длину отрезка"""
 
     point1: Point
@@ -23,13 +19,13 @@ class LineSegment:
 
     @property
     def length_line(self) -> float:
-
         """Длина отрезка d2= (х2— х1)2+ (y2— y1)2"""
+        dx = self.point2.x - self.point1.x
+        dy = self.point2.y - self.point1.y
+        return round((dx**2 + dy**2)**0.5, 1)
 
-        return round(((self.point2.x - self.point1.x) ** 2 + (self.point2.y - self.point1.y) ** 2) ** 0.5, 1)
 
 class Polygon(list):
-
     """Определяет многоугольник"""
 
     def __init__(self: list[Point]):
@@ -37,16 +33,13 @@ class Polygon(list):
 
     @property
     def perimeter(self):
-
         """Вычисляет периметр многоугольника при наличии трёх и более точек"""
-
         if len(self) < 3:
             raise ValueError('Задайте минимум три точки для построения многоугольника')
-
         else:
             return sum(
                 LineSegment(p1, p2).length_line
-                for p1, p2 in pairwise(self+[self[0]])
+                for p1, p2 in pairwise(self + [self[0]])
             )
 
 
