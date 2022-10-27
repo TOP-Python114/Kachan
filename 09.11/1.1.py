0b92a3fa51a553e45b0cb38db4ea2873668dbe22
KachanAnna <82448186+KachanAnna@users.noreply.github.com>
2022-10-27 23:59:33 +0700

    Update 1.1.py

--- 09.11/1.1.py
+++ 09.11/1.1.py
@@ -30,6 +30,11 @@ class Note:
         clone_obj.__dict__.update(**kwargs)
         return clone_obj
 
+    def __str__(self):
+        return (f'Высота: {self.pitch}\nОктава: {self.octave}'
+                f'\nAccidental: {self.accidental}'
+                f'\nПродолжительность: {self.duration}')
+
 
 class ScoreNote(Note):
     """Отображает музыкальную ноту в партитуре."""
@@ -47,6 +52,9 @@ class ScoreNote(Note):
         self.stem_up = stem_up
         self.beam = beam
 
+    def __str__(self):
+        return super().__str__()
+
 
 class MIDINote(Note):
     """Кодирует музыкальную ноту в MIDI протоколе."""
@@ -62,17 +70,45 @@ class MIDINote(Note):
         super().__init__(pitch=pitch, octave=octave, accidental=accidental, duration=duration)
         self.velocity = velocity
 
+    def __str__(self):
+        return super().__str__()+ f'\nVelocity: {self.velocity}'
 
-pot = Note(pitch=Pitch.C, octave=Octave.S_CONTRA, accidental=Accidental.NATURAL)
+note = Note(pitch=Pitch.C, octave=Octave.S_CONTRA, accidental=Accidental.NATURAL)
 
 midi_c3 = MIDINote(pitch=Pitch.C, octave=Octave.LINE_1, velocity=80)
-midi_d3 = midi_c3.clone(pitch=Pitch.D, octave=Octave.S_CONTRA)
+midi_d4 = midi_c3.clone(pitch=Pitch.D, octave=Octave.S_CONTRA)
 
-print(midi_d3.clone().pitch)
 
+print(f'Нота 1:')
+print(note, '\n')
+
+print(f'Нота 2:')
+print(midi_c3, '\n')
+
+print(f'Нота с измененными аттрибутами:')
+print(midi_d4, '\n')
 
 # ДОБАВИТЬ: под меткой tests закомментированные результаты выполнения скрипта с различными входными данными
 # tests:
+# Нота 1:
+# Высота: 1
+# Октава: -1
+# Accidental: natural
+# Продолжительность: Duration.QUARTER
+#
+# Нота 2:
+# Высота: 1
+# Октава: 3
+# Accidental: None
+# Продолжительность: Duration.QUARTER
+# Velocity: 80
+#
+# Нота с измененными аттрибутами:
+# Высота: 2
+# Октава: -1
+# Accidental: None
+# Продолжительность: Duration.QUARTER
+# Velocity: 80
 
 
 # ИТОГ: очень хорошо — 5/6
