c55cf0af18290706c535e784c5afebd7b76b342d
KachanAnna <82448186+KachanAnna@users.noreply.github.com>
2022-11-13 00:46:19 +0700

    ДЗ

---
+++ 09.25/1.1.py
@@ -0,0 +1,178 @@
+
+#У вас есть класс, в котором
+#находится несколько методов, вызов которых
+#вы хотите контролировать.
+#Спроектируйте и реализуйте модель классов
+#по шаблону Команда, обеспечивающих
+#следующую функциональность:
+#1) хранение истории операций
+#2) отмена последних операций
+#3) повторного выполнения отменённыx операций
+#Продемонстрируйте на тестовом классе ниже:
+
+
+from random import randrange as rr, choice as ch
+from string import ascii_lowercase as alc
+
+
+class TestCase:
+    def __init__(self):
+        self.messages = [
+            ''.join(ch(alc) for _ in range(rr(3, 6)))
+            for _ in range(1000)
+        ]
+        self.numbers = [
+            (rr(10, 100) for _ in range(rr(4, 7)))
+            for _ in range(1000)
+        ]
+        self.list = []
+        self.msg: str = ''
+        self.nums: int = 0
+
+    def print_msg(self):
+        """Генерирует строковый элемент"""
+
+        self.msg = self.messages.pop()
+        self.list.append(self.msg)
+        print(self.msg)
+        return self.list
+
+    def sum_nums(self):
+        """Генерирует числовой элемент"""
+
+        nums = self.numbers.pop()
+        self.number = sum(nums)
+        self.list.append(self.number)
+        print(self.number)
+        return self.list
+
+    def history(self):
+        """Показывает текущий набор элементов"""
+
+        print(self.list)
+
+    def undo(self):
+        """Удаляет последний элемент"""
+
+        self.last = self.list.pop()
+        return self.list
+
+    def redo(self):
+        """Отменяет удаление последнего элемента"""
+
+        self.list.append(self.last)
+        return self.list
+
+
+class Command:
+    """Базовый класс"""
+    def execute(self):
+        raise NotImplementedError()
+
+
+class CommandBase(Command):
+    """Класс, принимающий объект TestCase"""
+    def __init__(self, element):
+        self.element = element
+
+
+class AdditionMsg(CommandBase):
+    def execute(self):
+        """Вызывает метод, формирующий строковый элемент"""
+
+        self.element.print_msg()
+
+
+class AdditionNum(CommandBase):
+    def execute(self):
+        """Вызывает метод, формирующий числовой элемент"""
+
+        self.element.sum_nums()
+
+
+class RedoCommand(CommandBase):
+    def execute(self):
+        """Вызывает метод, осуществляющий повторное выполнение отмененной операции"""
+
+        self.element.redo()
+
+
+class UndoCommand(CommandBase):
+    def execute(self):
+        """Вызывает метод, отменяющий последнюю операцию"""
+        self.element.undo()
+
+
+class HistoryCommand(CommandBase):
+    def execute(self):
+        """Вызывает метод, для отображения текущих элементов"""
+        self.element.history()
+
+
+class AllComands:
+    """Класс формирует пул из команд"""
+
+    def __init__(self, addmsg, addnum, undo, redo, history):
+        self.addmsg = addmsg
+        self.addnum = addnum
+        self.undo = undo
+        self.redo = redo
+        self.history = history
+
+    def add_msg(self):
+        """Вызывает метод, формирующий строковый элемент"""
+
+        self.addmsg.execute()
+
+    def add_num(self):
+        """Вызывает метод, формирующий числовой элемент"""
+        self.addnum.execute()
+
+    def redo_com(self):
+        """Вызывает метод, осуществляющий повторное выполнение отмененной операции"""
+
+        self.redo.execute()
+
+    def undo_com(self):
+        """Вызывает метод, отменяющий последнюю операцию"""
+
+        self.undo.execute()
+
+    def history_com(self):
+        """Вызывает метод, для отображения текущих элементов"""
+
+        self.history.execute()
+
+
+test = TestCase()
+commands = AllComands(addmsg=AdditionMsg(test),
+                addnum=AdditionNum(test),
+                undo=UndoCommand(test),
+                redo=RedoCommand(test),
+                history=HistoryCommand(test)
+                      )
+
+commands.add_msg()
+commands.add_msg()
+commands.add_num()
+commands.history_com()
+commands.undo_com()
+commands.history_com()
+commands.undo_com()
+commands.history_com()
+commands.redo_com()
+commands.history_com()
+commands.add_msg()
+commands.add_num()
+commands.history_com()
+
+# syq
+# nooay
+# 266
+# ['syq', 'nooay', 266]
+# ['syq', 'nooay']
+# ['syq']
+# ['syq', 'nooay']
+# wxh
+# 217
+# ['syq', 'nooay', 'wxh', 217]
---
+++ 09.25/2.1.py
@@ -0,0 +1,194 @@
+#У вас есть веб-сервер, принимающий и обрабатывающий запросы. Каждый запрос после обработки получает ответ с определённым кодом.
+#Этот ответ должен пройти цепочку обработчиков, каждый из которых реагирует только на один определённый код.
+#Напишите класс, имитирующий получение веб-сервером запросов (например, из stdin) и генерацию ответов с кодами (например: 200, 403, 404, 500).
+#Ответ может быть кортежем, словарём, отдельным классом или любой другой структурой данных — главное, чтобы одним из элементов этой структуры был код ответа.
+#Напишите классы-обработчики в количестве, соответствующем числу генерируемых вариантов кодов ответа.
+#Каждый обработчик должен проверять код ответа.
+#Если код в ответе не соответствует коду конкретного обработчика, то обработчик ничего не делает с ответом, а передаёт объект ответа дальше (работа цепочки продолжается).
+#Если код в ответе эквивалентен коду конкретного обработчика, то обработчик должен обозначить свою реакцию (например, сообщением в stdout, содержащим имя класса обработчика) и НЕ должен передавать объект дальше (работа цепочки останавливается).
+#Самостоятельно выберите способ реализации: цепочка методов или брокер событий + CQS.
+from typing import Optional
+#вариант 1
+class Handler:
+    """Класс обработчика"""
+    def handle(self, code):
+        raise NotImplementedError()
+
+
+class Http404handler(Handler):
+    """Обработчик для кода 404"""
+    def handle(self, code) -> str:
+        if code == 404:
+            return 'Страница не найдена'
+
+class Http401handler(Handler):
+    """Обработчик для кода 401"""
+    def handle(self, code) -> str:
+        if code == 401:
+            return 'Неавторизированный запрос'
+
+
+class Http200handler(Handler):
+    """Обработчик для кода 200"""
+    def handle(self, code) -> str:
+        if code == 200:
+            return 'Запрос выполнен успешно'
+
+class Http403handler(Handler):
+    """Обработчик для кода 403"""
+    def handle(self, code) -> str:
+        if code == 403:
+            return 'Доступ к ресурсу запрещен'
+
+class Http500handler(Handler):
+    """Обработчик для кода 500"""
+    def handle(self, code) -> str:
+        if code == 500:
+            return 'Ошибка сервера'
+
+class Client:
+    """Класс клиент"""
+    def __init__(self):
+        self.__handlers = []
+
+    def add_handler(self, item) -> None:
+        self.__handlers.append(item)
+
+    def answer(self, code) -> str:
+        for item in self.__handlers:
+            msg = item.handle(code)
+            if msg:
+                print(msg)
+                break
+        else:
+            print('Запрос не авторизирован')
+
+
+client = Client()
+client.add_handler(Http401handler())
+client.add_handler(Http403handler())
+client.add_handler(Http404handler())
+client.add_handler(Http200handler())
+client.add_handler(Http500handler())
+i = 0
+while i < 10:
+    code = int(input('Введите код ошибки \n'))
+    client.answer(code)
+    i += 1
+# Введите код ошибки
+# 500
+# Ошибка сервера
+# Введите код ошибки
+# 5
+# Запрос не авторизирован
+# Введите код ошибки
+# 8
+# Запрос не авторизирован
+# Введите код ошибки
+# 401
+# Неавторизированный запрос
+# Введите код ошибки
+# 403
+# Доступ к ресурсу запрещен
+# Введите код ошибки
+# 800
+# Запрос не авторизирован
+# Введите код ошибки
+# 401
+# Неавторизированный запрос
+# Введите код ошибки
+# 403
+# Доступ к ресурсу запрещен
+# Введите код ошибки
+# 404
+# Страница не найдена
+# Введите код ошибки
+# 500
+# Ошибка сервера
+#################################################################################
+#вариант 2
+
+class Handler:
+    """Класс цепочки, запускает обработку данных."""
+    def __init__(self):
+        self.previous_modifier: Optional[Handler] = None
+        self.next_modifier: Optional[Handler] = None
+
+    def add_modifier(self, modifier: 'Handler'):
+        """Формирует звено цепочки."""
+        if self.next_modifier is None:
+            self.next_modifier = modifier
+            self.next_modifier.previous_modifier = self
+        else:
+            self.next_modifier.add_modifier(modifier)
+
+    def handle(self, code):
+        if self.next_modifier:
+            self.next_modifier.handle(code)
+
+
+class Http404handler(Handler):
+    """Обработчик для кода 404"""
+    def handle(self, code) -> str:
+        if code == 404:
+            print('Страница не найдена')
+        super().handle(code)
+
+class Http401handler(Handler):
+    """Обработчик для кода 401"""
+    def handle(self, code) -> str:
+        if code == 401:
+            print('Неавторизированный запрос')
+        super().handle(code)
+
+class Http200handler(Handler):
+    """Обработчик для кода 200"""
+    def handle(self, code) -> str:
+        if code == 200:
+            print('Запрос выполнен успешно')
+        super().handle(code)
+
+class Http403handler(Handler):
+    """Обработчик для кода 403"""
+    def handle(self, code) -> str:
+        if code == 403:
+            print('Доступ к ресурсу запрещен')
+        super().handle(code)
+
+class Http500handler(Handler):
+    """Обработчик для кода 500"""
+    def handle(self, code) -> str:
+        if code == 500:
+            print('Ошибка сервера')
+        super().handle(code)
+
+
+root = Handler()
+root.add_modifier(Http500handler())
+root.add_modifier(Http200handler())
+root.add_modifier(Http401handler())
+root.add_modifier(Http404handler())
+root.add_modifier(Http403handler())
+i = 0
+while i < 6:
+    code = int(input('Введите код ошибки \n'))
+    root.handle(code)
+    i += 1
+# Введите код ошибки
+# 500
+# Ошибка сервера
+# Введите код ошибки
+# 200
+# Запрос выполнен успешно
+# Введите код ошибки
+# 403
+# Доступ к ресурсу запрещен
+# Введите код ошибки
+# 401
+# Неавторизированный запрос
+# Введите код ошибки
+# 200
+# Запрос выполнен успешно
+# Введите код ошибки
+# 500
+# Ошибка сервера
