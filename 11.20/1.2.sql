3f58a26cdeaac53dd7d31f2a6adc77f0af2ce706
Gennadiy <renardes@gmail.com>
2023-01-30 14:52:25 +0500

    11.20: правки удобства чтения кода

--- 11.20/1.2.sql
+++ 11.20/1.2.sql
@@ -1,52 +1,50 @@
-/*Задание 2
-Создайте базу данных «Продажи». База данных должна
-содержать информацию о продавцах, покупателях, про-
-дажах. Необходимо хранить следующую информацию:
-■■ о продавцах: ФИО, email, контактный телефон;
-■■ о покупателях: ФИО, email, контактный телефон;
-■■ о продажах: покупатель, продавец, название товара,
-цена продажи, дата сделки.
-Для создания базы данных используйте запрос CREATE
-DATABASE. Для создания таблицы используйте запрос
-CREATE TABLE. Обязательно при создании таблиц зада-
-вать связи между ними.*/
+/*2. Создайте базу данных «Продажи». База данных должна содержать информацию о продавцах, покупателях, продажах. Необходимо хранить следующую информацию:
+ - о продавцах: ФИО, email, контактный телефон;
+ - о покупателях: ФИО, email, контактный телефон;
+ - о продажах: покупатель, продавец, название товара, цена продажи, дата сделки.*/
 
 create database if not exists sales;
+
 use sales;
+
 create table if not exists `Sellers` (
-	`Id` mediumint unsigned not null auto_increment,
+    `Id` mediumint unsigned not null auto_increment,
     `Name` varchar(30) not null,
     `Surname` varchar(30) not null,
-     `Phone` varchar(20) not null,
-     `Email` varchar(30),
-    constraint `PK_id` primary key(`Id`),
-	constraint `CH_Sellers_Name` check (`Name` <> ''),
+    `Phone` varchar(20) not null,
+    `Email` varchar(30),
+    constraint `PK_id` primary key (`Id`),
+    constraint `CH_Sellers_Name` check (`Name` <> ''),
     constraint `CH_Sellers_Surname` check (`Surname` <> '')
 );
+
 create table if not exists `Buyers` (
-	`Id` mediumint unsigned not null auto_increment,
+    `Id` mediumint unsigned not null auto_increment,
     `Name` varchar(30) not null,
     `Surname` varchar(30) not null,
-     `Phone` varchar(20) not null,
-     `Email` varchar(30),
-    constraint `PK_id` primary key(`Id`),
+    `Phone` varchar(20) not null,
+    `Email` varchar(30),
+    constraint `PK_id` primary key (`Id`),
     constraint `CH_Buyers_Name` check (`Name` <> ''),
     constraint `CH_Buyers_Surname` check (`Surname` <> '')
 );
+
 create table if not exists `Sale` (
-	`Id` mediumint unsigned not null auto_increment,
+    `Id` mediumint unsigned not null auto_increment,
     `Date` date not null,
     `Name` varchar(30) not null,
-     `Price` decimal(8,2) not null,
-     `Sellerid` mediumint unsigned not null,
-     `Buyerid` mediumint unsigned not null,
-    constraint `PK_id` primary key(`id`),
+    `Price` decimal(8,2) not null,
+    `SellerId` mediumint unsigned not null,
+    `BuyerId` mediumint unsigned not null,
+    constraint `PK_id` primary key (`id`),
     constraint `CH_Sales_Name` check (`Name` <> ''),
     constraint `CH_Sales_Price` check (`Price` > 0)
 );
 
 alter table `Sale` 
-	add constraint `FK_Sale_Sellerid` foreign key(`Sellerid`) 
-								  references `Sellers`(`Id`),
-	add constraint `FK_Sale_Buyerid` foreign key(`Buyerid`) 
-								references `Buyers`(`Id`);
+    add constraint `FK_Sale_SellerId`
+        foreign key (`SellerId`)
+        references `Sellers` (`Id`),
+    add constraint `FK_Sale_BuyerId`
+        foreign key (`BuyerId`)
+        references `Buyers` (`Id`);
--- 11.20/1.3.sql
+++ 11.20/1.3.sql
@@ -1,73 +1,76 @@
-/*3.Создайте базу данных «Музыкальная коллекция». База
-данных должна содержать информацию о музыкальных
-дисках, исполнителях, стилях. Необходимо хранить сле-
-дующую информацию:
-■■ о музыкальном диске: название диска, исполнитель,
-дата выпуска, стиль, издатель;
-■■ о стилях: названия стилей;
-■■ об исполнителях: название;
-■■ об издателях: название, страна;
-■■ о песнях: название песни, название диска, длитель-
-ность песни, музыкальный стиль песни, исполнитель.*/
+/*3. Создайте базу данных «Музыкальная коллекция». База данных должна содержать информацию о музыкальных дисках, исполнителях, стилях. Необходимо хранить следующую информацию:
+ - о музыкальном диске: название диска, исполнитель, дата выпуска, стиль, издатель;
+ - о стилях: названия стилей;
+ - об исполнителях: название;
+ - об издателях: название, страна;
+ - о песнях: название песни, название диска, длительность песни, музыкальный стиль песни, исполнитель.*/
 
 create database if not exists music;
+
 use music;
+
 create table if not exists `Disks` (
-	`Id` mediumint unsigned not null auto_increment,
+    `Id` mediumint unsigned not null auto_increment,
     `Name` varchar(30) not null,
     `ArtistId` mediumint unsigned not null,
     `PublisherId` mediumint unsigned not null,
     `GenreId` mediumint unsigned not null,
-     `Date` date,
-    constraint `PK_id` primary key(`Id`),
-	constraint `CH_Disks_Name` check (`Name` <> '')
+    `Date` date,
+    constraint `PK_id` primary key (`Id`),
+    constraint `CH_Disks_Name` check (`Name` <> '')
 );
 
 create table if not exists `Songs` (
-	`Id` mediumint unsigned not null auto_increment,
+    `Id` mediumint unsigned not null auto_increment,
     `Name` varchar(30) not null,
     `DiskId` mediumint unsigned not null,
     `ArtistId` mediumint unsigned not null,
     `Duration` smallint,
     `GenreId` mediumint unsigned not null,
-    constraint `PK_id` primary key(`Id`),
-	constraint `CH_Songs_Name` check (`Name` <> '')
+    constraint `PK_id` primary key (`Id`),
+    constraint `CH_Songs_Name` check (`Name` <> '')
 );
 
 create table if not exists `Genres` (
-	`Id` mediumint unsigned not null auto_increment,
+    `Id` mediumint unsigned not null auto_increment,
     `Name` varchar(30) not null,
-    constraint `PK_id` primary key(`Id`),
-	constraint `CH_Genres_Name` check (`Name` <> '')
+    constraint `PK_id` primary key (`Id`),
+    constraint `CH_Genres_Name` check (`Name` <> '')
 );
 
 create table if not exists `Artists` (
-	`Id` mediumint unsigned not null auto_increment,
+    `Id` mediumint unsigned not null auto_increment,
     `Name` varchar(30) not null,
-    constraint `PK_id` primary key(`Id`),
-	constraint `CH_Artists_Name` check (`Name` <> '')
+    constraint `PK_id` primary key (`Id`),
+    constraint `CH_Artists_Name` check (`Name` <> '')
 );
 
 create table if not exists `Publishers` (
-	`Id` mediumint unsigned not null auto_increment,
+    `Id` mediumint unsigned not null auto_increment,
     `Name` varchar(30) not null,
     `Country` varchar(30),
-    constraint `PK_id` primary key(`Id`),
-	constraint `CH_Publishers_Name` check (`Name` <> '')
+    constraint `PK_id` primary key (`Id`),
+    constraint `CH_Publishers_Name` check (`Name` <> '')
 );
 
 alter table `Disks` 
-	add constraint `FK_Disks_ArtistId` foreign key(`ArtistId`) 
-								  references `Artists`(`Id`),
-	add constraint `FK_Disks_PublisherId` foreign key(`PublisherId`) 
-								references `Publishers`(`Id`),
-	add constraint `FK_Disks_GenreId` foreign key(`GenreId`) 
-								references `Genres`(`Id`);
+    add constraint `FK_Disks_ArtistId`
+        foreign key (`ArtistId`)
+        references `Artists` (`Id`),
+    add constraint `FK_Disks_PublisherId`
+        foreign key (`PublisherId`)
+        references `Publishers` (`Id`),
+    add constraint `FK_Disks_GenreId`
+        foreign key (`GenreId`) 
+        references `Genres` (`Id`);
 
 alter table `Songs` 
-	add constraint `FK_Songs_ArtistId` foreign key(`ArtistId`) 
-								  references `Artists`(`Id`),
-	add constraint `FK_Songs_DiskId` foreign key(`DiskId`) 
-								references `Disks`(`Id`),
-	add constraint `FK_Songs_GenreId` foreign key(`GenreId`) 
-								references `Genres`(`Id`);
+    add constraint `FK_Songs_ArtistId`
+        foreign key (`ArtistId`)
+        references `Artists` (`Id`),
+    add constraint `FK_Songs_DiskId` 
+        foreign key (`DiskId`)
+        references `Disks` (`Id`),
+    add constraint `FK_Songs_GenreId`
+        foreign key (`GenreId`)
+        references `Genres` (`Id`);
